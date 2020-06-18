5114219a2e103da52becdd8a044f95cfcb8111d8
devWRM <mcwen@comcast.net>
2020-06-17 22:22:24 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+require 'pry'
+#  VIDEO:       https://www.youtube.com/watch?v=33pLqGvk-PM
+#  VIDEO:       https://www.youtube.com/watch?v=xXuEeChktdQ
+
+# prime_numbers = Prime.take_while {|num| num < 1000}
+
+
+def prime?(number)
+    if number.negative? || number == 0 || number == 1
+
+        return false
+    else
+        (2..number-1).none? {|divisor| number % divisor == 0}
+    end
+end
+
+
