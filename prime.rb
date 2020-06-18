be5ddf4b6b7bc2ce5e6438c78055da3fd5057e86
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-18 20:01:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+require "pry"
+def prime?(num)
+  if num <= 0 || num == 1
+    return false
+  end
+  nums = (2..(num - 1)).to_a
+  ans = !nums.any? { |val| num % val == 0 } 
+  ans
+end
