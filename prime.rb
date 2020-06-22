3cdd3e474054ecf1fabc0846ccbb2f7b8d626711
Lisa Feng <lisafeng10@gmail.com>
2020-06-22 14:38:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+require "pry"
+
+def prime?(number)
+  if number == 2
+    return true
+  elsif number == 1||number == -1||number == 0
+    return false
+  elsif number < -1
+    number = number * -1
+    i = (2..(number - 1)).to_a
+    i.each do |divisor|
+      if number % divisor == 0
+        return false
+      end
+    end
+    true
+  else
+    i = (2..(number - 1)).to_a
+    i.each do |divisor|
+      if number % divisor == 0
+        return false
+      end
+    end
+    true
+  end
+end
