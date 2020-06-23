7671b6758fa143c7d73865e25b92fa402051d38d
Jose Hidago <jhidalgo521@gmail.com>
2020-06-23 14:14:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,27 @@
 # Add  code here!
+
 def prime?(number)
+  
   start = 2
-  if number > 1
+  
+# prime numbers are natural numbers greater than 1
+  
+  if number > 1 
+   
+# prime number solution is to iterate range of numbers from 2 to nth - 1
+    
+    
     range = (start..number-1).to_a
-    range.none? do |num_to_test| 
+    range.none? do |test_prime| 
       
-      number % num_to_test == 0
+# next part is to check for any remainders, if its 0 that means its fully divisable and not prime!
+
+
+    number % test_prime == 0
     end
   else
     false
   end
 end
+
+# code isnt pretty but i use the comments to keep me focused on each portion of the problem
