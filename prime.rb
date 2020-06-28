24bf6ffa39a04b071acb607ee0eca5f1516d968e
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 18:26:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  test = (2..number-1).all? {|num| number%num != 0}
+  if test == number.negative?()
+    return false
+  elsif number == 0
+    return false
+  elsif number == 1
+    return false
+  else
+    return test
+  end
+end
+
+
